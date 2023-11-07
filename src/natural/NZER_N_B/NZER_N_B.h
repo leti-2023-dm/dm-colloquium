c4bc92cbc501e2826a306937fee968b3fd85336a
MedvedsckyDaniil <112641584+MedvedsckyDaniil@users.noreply.github.com>
2023-11-07 19:42:38 +0300

    task/NZER_N_B
    Не моё, но NZER_N_B попрошу взять именно мой, потому что в нём я сделал дополнительную функцию bool NZER_N_B::get_bool(Natural* obj) для удобства написания других модулей.

---
+++ src/natural/NZER_N_B/NZER_N_B.h
@@ -0,0 +1,13 @@
+#ifndef LONG_OPERATIONS_NZER_N_B_H
+#define LONG_OPERATIONS_NZER_N_B_H
+#include "../../operation/operation1Natural.h"
+
+
+
+class NZER_N_B : Operation1Natural {
+public:
+    Natural* get(Natural* obj) const final;
+    bool get_bool(Natural* obj) const;
+};
+
+#endif //LONG_OPERATIONS_NZER_N_B_H
