a6d4e9a8d86f3ee117e6f3bbc59d3cce625cc9a9
Michael Dudkin <134552025+MikHalo-Dudino@users.noreply.github.com>
2023-11-08 22:50:16 +0300

    Update TRANS_N_Z.h

--- src/integers/TRANS_N_Z/TRANS_N_Z.h
+++ src/integers/TRANS_N_Z/TRANS_N_Z.h
@@ -3,7 +3,7 @@
 #include "../../operation/operation1.h"
 #include "../../algebra/integer.h"
 class TRANS_N_Z : public Operation1 {
-	Algebra* TRANS_N_Z::get(Algebra* object) const;
-	Integer* TRANS_N_Z::get(Natural* object)
+	Algebra* get(Algebra* object) const;
+	Integer* get(Natural* object);
 };
 #endif
