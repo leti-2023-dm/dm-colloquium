9470875e5012cd770b7ad462b877bd38b59c1c1b
Michael Dudkin <134552025+MikHalo-Dudino@users.noreply.github.com>
2023-11-08 21:46:35 +0300

    Create MUL_ZM_Z.cpp

---
+++ src/integers/MUL_ZM_Z/MUL_ZM_Z.cpp
@@ -0,0 +1,12 @@
+#ifndef MUL_ZM_Z_cpp
+#define MUL_ZM_Z_cpp
+#include "MUL_ZM_Z.h"
+
+Integer* MUL_ZM_Z::get(Integer* obj) const
+{	
+	Integer* res = new Integer(obj);
+	res->changeSign();
+	return res;
+}
+
+#endif // !MUL_ZM_Z_cpp
