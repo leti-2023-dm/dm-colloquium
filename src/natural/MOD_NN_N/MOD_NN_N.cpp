32e86b969c7453c8980891afd3eb0fe3b9ecc9e3
Natalya00 <112632574+Natalya00@users.noreply.github.com>
2023-11-08 23:12:06 +0300

    Update MOD_NN_N.cpp

--- src/natural/MOD_NN_N/MOD_NN_N.cpp
+++ src/natural/MOD_NN_N/MOD_NN_N.cpp
@@ -2,8 +2,8 @@
 
 Natural* MOD_NN_N::get(Natural* firstOperand, Natural* secondOperand) const {
     DIV_NN_N divResult;
-	Natural* quotient = divResult.get(firstOperand, secondOperand);
+    Natural* quotient = divResult.get(firstOperand, secondOperand);
     SUB_NDN_N sub(quotient);
-  	Natural* subResult = sub.get(firstOperand, secondOperand);
-	return subResult;
+    Natural* subResult = sub.get(firstOperand, secondOperand);
+    return subResult;
 }
