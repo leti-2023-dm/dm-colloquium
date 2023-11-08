933d84653b747cdfaa34908b6361bedcdfcef5e0
Michael Dudkin <134552025+MikHalo-Dudino@users.noreply.github.com>
2023-11-08 15:29:11 +0300

    Create POZ_Z_D.h

---
+++ src/POZ_Z_D/POZ_Z_D.h
@@ -0,0 +1,11 @@
+#ifndef POZ_Z_D_h
+#define POZ_Z_D_h
+#include "../../operation/operation1Integer.h"
+
+class POZ_Z_D : public Operation1Integer { // function tests sign of number
+public:
+    Integer* get(Integer* obj) const;
+};
+
+
+#endif
