30d650adf6682f5c422de8f789018f0b1d5f62bd
MedvedsckyDaniil <112641584+MedvedsckyDaniil@users.noreply.github.com>
2023-11-09 15:14:06 +0300

    Update NZER_N_B.cpp

--- src/natural/NZER_N_B/NZER_N_B.cpp
+++ src/natural/NZER_N_B/NZER_N_B.cpp
@@ -1,7 +1,15 @@
 #include "NZER_N_B.h"
 
-Natural* NZER_N_B::get(Natural* obj) const{
-    if(obj->len() == 1 && obj->get(0) == 0)
+Natural* NZER_N_B::get(Natural* obj) const {
+    if (obj->len() == 1 && obj->get(0) == 0){
         return new Natural("0");
-    return new Natural("1");
+    }
+    else {
+        return new Natural("1");
+    }
+}
+
+bool NZER_N_B::get_bool(Natural* obj) const {
+    // if (obj == 0) return 0; else return 1;
+    return !(obj->len() == 1 && obj->get(0) == 0);
 }
