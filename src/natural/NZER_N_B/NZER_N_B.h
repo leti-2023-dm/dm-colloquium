aa11b45913c1c30a5b669c2d5f993eb38312e30f
MedvedsckyDaniil <112641584+MedvedsckyDaniil@users.noreply.github.com>
2023-11-09 15:09:16 +0300

    Update NZER_N_B.h

--- src/natural/NZER_N_B/NZER_N_B.h
+++ src/natural/NZER_N_B/NZER_N_B.h
@@ -1,10 +1,13 @@
-#ifndef NZERNB
-#define NZERNB
+#ifndef LONG_OPERATIONS_NZER_N_B_H
+#define LONG_OPERATIONS_NZER_N_B_H
 #include "../../operation/operation1Natural.h"
 
-class NZER_N_B: public Operation1Natural{
+
+
+class NZER_N_B : Operation1Natural {
 public:
-    Natural* get(Natural* obj) const;
+    Natural* get(Natural* obj) const final;
+    bool get_bool(Natural* obj) const;
 };
 
-#endif
+#endif //LONG_OPERATIONS_NZER_N_B_
