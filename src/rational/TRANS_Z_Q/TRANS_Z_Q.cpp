cfe76c3ede3df0c53e6d90bffd4f515aa6fdad80
MedvedsckyDaniil <112641584+MedvedsckyDaniil@users.noreply.github.com>
2023-11-09 15:22:00 +0300

    Update TRANS_Z_Q.cpp

--- src/rational/TRANS_Z_Q/TRANS_Z_Q.cpp
+++ src/rational/TRANS_Z_Q/TRANS_Z_Q.cpp
@@ -1,5 +1,9 @@
 #include "TRANS_Z_Q.h"
 
 Rational* TRANS_Z_Q::get(Integer* operationObject) const {
-    return new Rational(*operationObject, Natural("1"));
+    return new Rational(*operationObject);
+}
+
+Algebra* TRANS_Z_Q::get(Algebra* operationObject) const {
+    return new Rational(*operationObject);
 }
