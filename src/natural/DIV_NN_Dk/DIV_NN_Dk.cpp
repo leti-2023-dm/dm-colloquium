38c264db00a406fc9dfa0d3efea67a8f3b4b55fc
mrekk-hub <maxim.shlyahtin@yandex.ru>
2023-11-11 17:07:14 +0300

    applied suggested changes

--- src/natural/DIV_NN_Dk/DIV_NN_Dk.cpp
+++ src/natural/DIV_NN_Dk/DIV_NN_Dk.cpp
@@ -6,7 +6,7 @@ Natural* DIV_NN_Dk::get(Natural* firstOperand, Natural* secondOperand) const{
     size_t secondOperandLen = secondOperand->len();
     Natural firstDigit("1");
     size_t k;
-    if(firstOperandLen < secondOperandLen){
+    if(COM_NN_D().get(firstOperand, secondOperand)->get(0) == 1){
         return new Natural("0"); // the second operand is supposed to be smaller than the first
     } else if(COM_NN_D().get(firstOperand, secondOperand)->get(0) == 0) {
         return new Natural("1"); // the operands are equal
@@ -17,7 +17,7 @@ Natural* DIV_NN_Dk::get(Natural* firstOperand, Natural* secondOperand) const{
         for(k = firstOperandLen - 1; k >= 0; --k){
             Natural divisible(*firstOperand, k, firstOperandLen); // adding another digit
             while(COM_NN_D().get(&divisible, divider)->get(0) == 2){
-                divider = MUL_ND_N().get(&divider, &firstDigit);
+                divider = MUL_ND_N().get(secondOperand, &firstDigit);
                 firstDigit.set(0, firstDigit.get(0) + 1);
                 flag = true;
             }
